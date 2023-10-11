2f12e00c43f1ba76289efd98973ece1dc44366b6
mMahnoor <mahnurakther@gmail.com>
2023-10-11 19:51:03 +0600

    added navigation bar

---
+++ client/src/components/navbar.jsx
@@ -0,0 +1,123 @@
+import React from "react";
+// import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+import {
+  Navbar as MTNavbar,
+  Collapse,
+  Typography,
+  Button,
+  IconButton,
+} from "@material-tailwind/react";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
+
+export function Navbar({ brandName, routes, action }) {
+  const [openNav, setOpenNav] = React.useState(false);
+ 
+  React.useEffect(() => {
+    window.addEventListener(
+      "resize",
+      () => window.innerWidth >= 960 && setOpenNav(false)
+    );
+  }, []);
+
+  const navList = (
+    <ul className="mb-4 mt-2 flex flex-col gap-2 text-inherit lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6">
+      {routes.map(({ name, path, icon, href, target }) => (
+        <Typography
+          key={name}
+          as="li"
+          variant="small"
+          color="inherit"
+          className="capitalize"
+        >
+          {href ? (
+            <a
+              href={href}
+              target={target}
+              className="flex items-center gap-1 p-1 font-normal"
+            >
+              {icon &&
+                React.createElement(icon, {
+                  className: "w-[18px] h-[18px] opacity-75 mr-1",
+                })}
+              {name}
+            </a>
+          ) : (
+            <Link
+              to={path}
+              target={target}
+              className="flex items-center gap-1 p-1 font-normal"
+            >
+              {icon &&
+                React.createElement(icon, {
+                  className: "w-[18px] h-[18px] opacity-75 mr-1",
+                })}
+              {name}
+            </Link>
+          )}
+        </Typography>
+      ))}
+    </ul>
+  );
+
+  return (
+    <MTNavbar color="transparent" className="sticky top-0 z-10 h-max max-w-full rounded-none py-2 px-2 lg:px-8 lg:py-4">
+      <div className="container mx-auto flex items-center justify-between text-white">
+        <Link to="/">
+          <Typography className="mr-4 ml-2 cursor-pointer py-1.5 font-bold">
+            {brandName}
+          </Typography>
+        </Link>
+        <div className="hidden lg:block">{navList}</div>
+        <div className="hidden gap-2 lg:flex">
+          
+          {React.cloneElement(action, {
+            className: "hidden lg:inline-block",
+          })}
+        </div>
+        <IconButton
+          variant="text"
+          size="sm"
+          color="white"
+          className="ml-auto text-inherit hover:bg-transparent focus:bg-transparent active:bg-transparent lg:hidden"
+          onClick={() => setOpenNav(!openNav)}
+        >
+          {openNav ? (
+            <XMarkIcon strokeWidth={2} className="h-6 w-6" />
+          ) : (
+            <Bars3Icon strokeWidth={2} className="h-6 w-6" />
+          )}
+        </IconButton>
+      </div>
+      <Collapse
+        className={openNav ? "rounded-xl bg-white px-4 pt-2 pb-4 text-blue-gray-900" : ""}
+        open={openNav}
+      >
+        <div className="container mx-auto">
+          {navList}
+          {React.cloneElement(action, {
+            className: "w-full block",
+          })}
+        </div>
+      </Collapse>
+    </MTNavbar>
+  );
+}
+
+Navbar.defaultProps = {
+  brandName: (
+    <img
+      src="/img/ru_new_logo.png"
+      alt="Logo"
+      width="100" 
+    />
+  ),
+  action: (
+    <Button variant="gradient" size="sm" fullWidth>
+      Explore now
+    </Button>
+   
+  ),
+};
+
+export default Navbar;
