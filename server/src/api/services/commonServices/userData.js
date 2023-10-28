4663e6bcee62e719a3ac540d30906a5a0e5bc6b8
mMahnoor <mahnurakther@gmail.com>
2023-10-28 11:29:02 +0600

    update on userData

--- server/src/api/services/commonServices/userData.js
+++ server/src/api/services/commonServices/userData.js
@@ -9,18 +9,10 @@ exports.userData = async({email, password})=>{
     const userInfo = await models.Users.findOne({"email":loginCreds.email}).exec();
     if(!userInfo) return ({ error: "User not found!" });
     if(userInfo.password!=loginCreds.password){
-        // return ({ error: "Wrong Password Combination!" });
         return ({ error: "Wrong Password Combination!" });
     } else{
         return userInfo;
-        // const accessToken = middleware.JWT.createTokens(loginCreds);
-
-        // res.cookie("access-token", accessToken, {
-        //     maxAge: 2592000,
-        //     httpOnly: true,
-        // });
-
-        // res.status(200).json(userInfo);
+        
     }
 
 }
