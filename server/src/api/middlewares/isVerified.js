643ff6928a4d6bcb5dadfea17bd3b4d56ae2eaa6
mMahnoor <mahnurakther@gmail.com>
2023-10-30 15:53:20 +0600

    chenged conditions

--- server/src/api/middlewares/isVerified.js
+++ server/src/api/middlewares/isVerified.js
@@ -4,6 +4,6 @@ exports.check = async(req, res, next) => {
     const email = req.body.email;
     const info = await models.Users.findOne({email: email}).exec();
     if(!info) return res.status(401).send("Unauthorized access!");
-    if(!info.isVerified) return res.status(401).send("Email not verified");
+    if(info.isVerified===false) return res.status(401).send("Email not verified");
     next();
 }
